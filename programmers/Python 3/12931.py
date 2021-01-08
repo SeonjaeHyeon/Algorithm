26f410e575f8652c525db686aad4f0b513d51b7a
SeonjaeHyeon <hsj106@mju.ac.kr>
2021-01-08 14:40:37 +0900

    Add solution of programmers No. 12931

---
+++ programmers/Python 3/12931.py
@@ -0,0 +1,20 @@
+'''
+https://programmers.co.kr/learn/courses/30/lessons/12931
+연습문제
+자릿수 더하기
+
+[문제설명]
+자연수 N이 주어지면, N의 각 자릿수의 합을 구해서 return 하는 solution 함수를 만들어 주세요.
+예를들어 N = 123이면 1 + 2 + 3 = 6을 return 하면 됩니다.
+
+[제한조건]
+N의 범위 : 100,000,000 이하의 자연수
+'''
+
+# List comprehension, sum(), int(), str() 사용
+def solution(n):
+    return sum([int(x) for x in str(n)])
+
+# sum(), map(), str() 사용
+def solution2(n):
+    return sum(map(int, str(n)))
